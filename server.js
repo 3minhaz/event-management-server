31c9c421389c85d937da3398d034c0eb5bce5191
Emon Islam <emonislamweb@gmail.com>
2022-01-02 19:50:39 +0600

    auth user login register added

--- server.js
+++ server.js
@@ -5,16 +5,16 @@ const cors = require('cors');
 const connectDb = require('./config/DB');
 const port = process.env.PORT || 5000;
 const app = express()
-
+//db connected
+connectDb();
+//import Custom Routes
+const userRoutes = require('./routes/userRoutes');
 //middleware
 app.use(cors());
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: false }));
-
-//db connected
-connectDb();
-//import Custom Routes
-
+//api calling
+app.use('/users',userRoutes)
 
 app.get('/',(req,res) =>{
     res.send("server running")
