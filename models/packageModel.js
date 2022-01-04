191799d3e015cb841a9adcc00f63e1bc97bd8fbf
1emonislam <emonislamweb@gmail.com>
2022-01-04 09:32:06 +0600

    price string

--- models/packageModel.js
+++ models/packageModel.js
@@ -13,7 +13,7 @@ const packageSchema = mongoose.Schema({
         required: true
     },
     price:{
-        type: Number,
+        type: String,
         required: true
     },
     categories: {
